d696d5e3ebb5aa0f9796ad52cd7925e36371d798
GokulGitConfig <signingokul@gmail.com>
2017-01-11 22:44:41 +0530

    2nd Commend line added to the model

--- MvcMovie/Models/ManageViewModels.cs
+++ MvcMovie/Models/ManageViewModels.cs
@@ -13,6 +13,7 @@ namespace MvcMovie.Models
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
+        //commend line 2
     }
 
     public class ManageLoginsViewModel
